74cb24465b30db823f51d857bd1846942af29eca
Max <86778428+aznmax@users.noreply.github.com>
2022-05-27 12:42:42 +0200

    end of of project

---
+++ Stars.jsx
@@ -0,0 +1,18 @@
+import StarSolid from "../media/star-solid.svg"
+import StarEmpty from "../media/star-empty.svg"
+
+
+ function Stars(props) {
+
+    return (
+        <div className="stars-content">
+            
+            {Array.from({length: 5 } , (item, itemKey) => itemKey < props.star ? <img key={itemKey} src={StarSolid} alt="star svg" className="starsolid" /> : <img key={itemKey} src={StarEmpty} alt="star svg" className="starsempty" />)}
+        </div>
+
+        
+    )
+
+}
+
+export default Stars
